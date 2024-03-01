03eae32237304a7f210199c193279041c4a2da96
NeilCaldwell <12889992+neilcaldwell@users.noreply.github.com>
2024-03-01 13:36:57 +1100

    adding dark mode override

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html class="dark" lang="en">
       <body>{children}</body>
     </html>
   )
